1fef961c13efa2dcbee8b1e4a4613b14d7d5f9d6
Your NameMickeyBro19 <bhaskarp986@gmail.com>
2026-07-02 09:44:53 +0530

    Initial commit feat: Created a new blank Spring-boot project to Initialize e-commerce backend project

---
+++ src/main/java/com/mickey/ecommercebackend/Project5Application.java
@@ -0,0 +1,13 @@
+package com.mickey.ecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project5Application.class, args);
+    }
+
+}
